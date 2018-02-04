a7bbc792813d756d79ae5e6f96782326f38983d1
Shravan Chitragar <shravan.chitragar@gmail.com>
2018-02-04 00:42:15 -0500

    First stage

---
+++ minesweeper.js
@@ -0,0 +1,12 @@
+const blankline= '  |   |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankline);
+console.log(blankline);
+console.log(blankline);
+
+const guessline = '1 |   |  ';
+const bombline = '  | B |  ';
+console.log('This is what an guessed board would look like:');
+console.log(guessline);
+console.log(bombline);
+console.log(blankline);
