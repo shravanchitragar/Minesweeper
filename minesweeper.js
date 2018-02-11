c0b4bbd8def86a8d061fdacd47deca265201c653
Shravan Chitragar <shravan.chitragar@gmail.com>
2018-02-11 16:27:03 -0500

    Second Stage

--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,52 @@
-const blankline= '  |   |  ';
-console.log('This is what an empty board would look like:');
-console.log(blankline);
-console.log(blankline);
-console.log(blankline);
+const generatePlayerBoard = (numberOfRows,numberOfColumns)=>{
+  let board = [];
+  for(let i =0; i<numberOfRows ; i++)
+  {
+    let row = [];
+    for(let j = 0 ; j < numberOfColumns ; j++)
+    {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
 
-const guessline = '1 |   |  ';
-const bombline = '  | B |  ';
-console.log('This is what an guessed board would look like:');
-console.log(guessline);
-console.log(bombline);
-console.log(blankline);
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs)=>{
+  let Bboard = [];
+  for(let i =0; i<numberOfRows ; i++)
+  {
+    let Brow = [];
+    for(let j = 0 ; j < numberOfColumns ; j++)
+    {
+      Brow.push(null);
+    }
+    Bboard.push(Brow);
+  }
+
+//let numberOfBombsPlaced = 0;
+
+  while(numberOfBombs>0)
+  {
+    let randomRowIndex = Math.floor(Math.random()*numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random()*numberOfColumns);
+    Bboard[randomRowIndex][randomColumnIndex] ='B';
+    numberOfBombs--;
+
+  }
+  return Bboard;
+};
+
+const printBoard = (board)=>{
+console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard  = generatePlayerBoard(3,4);
+let BombBoard  = generateBombBoard(3,4,5);
+
+console.log(printBoard(playerBoard));
+
+
+
+console.log(printBoard(BombBoard));
