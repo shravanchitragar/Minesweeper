3e5688ffbcdb6a980303683e1567304b201e2796
Shravan Chitragar <shravan.chitragar@gmail.com>
2018-02-18 16:23:30 -0500

    Final

--- board.js
+++ board.js
@@ -1,32 +1,3 @@
-class Game{
-  constructor(numberOfRows, numberOfColumns,numberOfBombs)
-  {
-    this._board = new Board(numberOfRows, numberOfColumns,numberOfBombs);
-  }
-
-  playMove(rowIndex,columnIndex)
-  {
-    this._board.flipTile(rowIndex,columnIndex);
-
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B')
-    {
-      console.log("Game is Over");
-      this._board.print();
-    }
-
-    else if(this._board.hasSafeTiles===false)
-    {
-      Console.log("Congratulations!!!");
-    }
-
-    else {
-      {
-        console.log("Current board: ");
-        this._board.print();
-      }
-    }
-  }
-};
 class Board{
   constructor(numberOfRows,numberOfColumns,numberOfBombs)
   {
@@ -138,3 +109,5 @@ class Board{
   }
 
 };
+
+module.exports = Board;
